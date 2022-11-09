6ee0a872c6a0b86d8011e9d54d1f4450d0fd60bc
Aman <amanmalik3105@gmail.com>
2022-11-09 20:12:29 +0530

    adding css and js

---
+++ joke.js
@@ -0,0 +1,27 @@
+const button = document.querySelector(".contain button");
+const jokeDiv = document.querySelector(".contain .joke p");
+
+document.addEventListener("DOMContentLoaded", getJock);
+
+button.addEventListener("click", getJock);
+
+async function getJock() {
+  const jokeData = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
+  });
+  const jokeObj = await jokeData.json();
+  jokeDiv.innerHTML = jokeObj.joke;
+  console.log(jokeData);
+}
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
